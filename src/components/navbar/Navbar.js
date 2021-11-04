cf180ad12626707c46a5935cdbb2207108e8dd0f
Ines <simonica.ines@gmail.com>
2021-11-04 11:09:52 +0200

    update cv link

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar(){
     return (
         <nav>
             <ul>
-                <li><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1wGXj99yfBwQBjwJjfzVR6yMsSe-KAhFT/view?fbclid=IwAR1OvZYHrKOnPkuZxo9S9VtafdaGO2Kr2cLUj8J71F96rBD920TRLcodEPs">CV</a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/1WqsOjOMMKj3DBms0iyV-guFN7JBEOHCZ/view?usp=sharing">CV</a></li>
                 <li><a href="#projects" onClick={() => scroll("projects")}>PROJECTS</a></li>
                 <li><a href="#contact" onClick={() => scroll("contact")}>CONTACT</a></li>
             </ul>
